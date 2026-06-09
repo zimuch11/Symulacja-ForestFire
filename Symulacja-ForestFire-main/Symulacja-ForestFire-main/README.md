68e2f274d0e7bacc595e7032b3163deab2660d6f
Frostoise11 <zimuch11@gmail.com>
2026-06-09 23:33:24 +0200

    Delete Symulacja-ForestFire-main/Symulacja-ForestFire-main directory

--- Symulacja-ForestFire-main/Symulacja-ForestFire-main/README.md
+++
@@ -1,1 +0,0 @@
-# Symulacja-Fire
--- Symulacja-ForestFire-main/Symulacja-ForestFire-main/Symulacja_Fire/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.symulacja_fire {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.symulacja_fire to javafx.fxml;
-    exports com.example.symulacja_fire;
-}
