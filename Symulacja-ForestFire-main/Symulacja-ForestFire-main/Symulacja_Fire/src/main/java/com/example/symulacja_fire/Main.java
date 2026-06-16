9c2fbf754a315c3a5b3e0313b9d88f073eae78be
iddhh <135729945+iddhh@users.noreply.github.com>
2026-06-16 20:51:58 +0200

    Delete Symulacja-ForestFire-main/Symulacja-ForestFire-main directory

--- Symulacja-ForestFire-main/Symulacja-ForestFire-main/Symulacja_Fire/src/main/java/com/example/symulacja_fire/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.symulacja_fire;
-
-public class Main {
-    public static void main(String args[]){
-        Simulation sim = new Simulation(300,200,0.7,5,0);
-        sim.initialize();
-        sim.run(300);
-    }
-}
